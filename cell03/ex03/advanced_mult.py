c411be5b855dea9f20b0081637dac0e770765883
zedl3all <sanguanwongtanapat@gmail.com>
2026-09-22 15:32:16 +0700

    Modify to match the test case

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -1,4 +1,11 @@
+import sys
+
 def main():
+
+    if len(sys.argv) != 1:
+        print("none")
+        return
+
     for i in range(11):
         print("Table de", str(i) + ": ", end="")
         for j in range(11):
