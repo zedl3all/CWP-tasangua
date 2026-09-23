fba3aa78b9b54ecd69bf50e8e10af006b4c3b176
OlDeuS <66070083@kmitl.ac.th>
2026-09-23 21:15:02 +0700

    bishop and pawn

--- miniproject/Chess/Bishop.py
+++ miniproject/Chess/Bishop.py
@@ -4,3 +4,21 @@ class Bishop(Piece):
 
     def move_check(self, board_size:list, King: object) -> bool:
         """ตรวจสอบว่าเดินแล้วโดน King ไหม"""
+        for i in range(1, max(board_size)):
+            # ทแยงซ้ายบน
+            if King.x == self.x - i and King.y == self.y - i:
+                return True
+
+            # ทแยงขวาบน
+            if King.x == self.x - i and King.y == self.y + i:
+                return True
+
+            # ทแยงซ้ายล่าง
+            if King.x == self.x + i and King.y == self.y - i:
+                return True
+
+            # ทแยงขวาล่าง
+            if King.x == self.x + i and King.y == self.y + i:
+                return True
+
+        return False
--- miniproject/Chess/Pawn.py
+++ miniproject/Chess/Pawn.py
@@ -4,3 +4,15 @@ class Pawn(Piece):
 
     def move_check(self, board_size:list, King: object) -> bool:
         """ตรวจสอบว่าเดินแล้วโดน King ไหม"""
+        next_row = self.x - 1
+        left_column = self.y - 1
+        right_column = self.y + 1
+
+        if King.x == next_row: 
+            if King.y == left_column:
+                return True
+
+            if King.y == right_column:
+                return True
+
+        return False
